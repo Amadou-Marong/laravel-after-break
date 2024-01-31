ca463dfb7a55b86f33f0c0c3c7ba591e15bbbfe9
Amadou-Marong <marongamadou6@gmail.com>
2024-01-31 06:04:46 +0000

    this is my hands on laravel afterbreak

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,13 +1,15 @@
 <template>
     <div v-for="listing in listings" :key="listing.id">
         <Link :href="`/listing/${listing.id}`">
-            {{ listing.street }}, {{ listing.street_nr }}, {{ listing.city }}, for {{ listing.price }}
+            <!-- {{ listing.street }}, {{ listing.street_nr }}, {{ listing.city }}, for {{ listing.price }} -->
+            <ListingAddress :listing="listing"/>
         </Link>
     </div>
 </template>
 
 <script setup>
     import { Link } from '@inertiajs/vue3';
+    import ListingAddress from '@/Components/ListingAddress.vue'
     defineProps({
         listings: Array,
     })
--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -1,13 +1,16 @@
 <template>
     <div>
         <Link>
-           {{ listing.street }}, {{ listing.street_nr }}, {{ listing.city }}, for {{ listing.price }}
+           <!-- {{ listing.street }}, {{ listing.street_nr }}, {{ listing.city }}, for {{ listing.price }} -->
+            <ListingAddress :listing="listing"/>
+
         </Link>
     </div>
 </template>
 
 <script setup>
     import { Link } from '@inertiajs/vue3';
+    import ListingAddress from '@/Components/ListingAddress.vue'
     defineProps({
         listing: Object
     })
