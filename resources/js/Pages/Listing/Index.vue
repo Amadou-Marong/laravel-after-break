108b63fd4bb67f7f21fced25001440d80a68c492
Amadou-Marong <marongamadou6@gmail.com>
2024-02-01 22:27:50 +0000

    this is where I updated the index page with box component

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,28 +1,19 @@
 <template>
-    <!-- <div v-for="listing in listings" :key="listing.id"> -->
-    <Box v-for="listing in listings" :key="listing.id">
-        <div>
-            <!-- <Link :href="`/listing/${listing.id}`"> -->
-
-             <!-- Now  we can use the route in the Link instead of the hard coded path -->
-            <!-- <Link :href="route('listing.show', listing.id)"> -->
-
-            <Link :href="route('listing.show', {listing: listing.id})">
-                <!-- {{ listing.street }}, {{ listing.street_nr }}, {{ listing.city }}, for {{ listing.price }} -->
-                <ListingAddress :listing="listing"/>
-            </Link>
-        </div>
-        <div>
-            <!-- <Link :href="`/listing/${listing.id}/edit`">Edit</Link> -->
-            
-            <Link :href="route('listing.edit', {listing: listing.id})">Edit</Link>
-        </div>
-        <div>
-            <!-- <Link :href="`/listing/${listing.id}`" method="DELETE" as="button">Delete</Link> -->
-
-            <Link :href="route('listing.destroy', {listing: listing.id})" method="DELETE" as="button">Delete</Link>
-        </div>
-    </Box>
+    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+        <Box v-for="listing in listings" :key="listing.id">
+            <div>
+                <Link :href="route('listing.show', {listing: listing.id})">
+                    <ListingAddress :listing="listing"/>
+                </Link>
+            </div>
+            <div>
+                <Link :href="route('listing.edit', {listing: listing.id})">Edit</Link>
+            </div>
+            <div>
+                <Link :href="route('listing.destroy', {listing: listing.id})" method="DELETE" as="button">Delete</Link>
+            </div>
+        </Box>
+    </div>
 </template>
 
 <script setup>
