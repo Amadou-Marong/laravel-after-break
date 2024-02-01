cfca32263ff10724c22f4151c0bbf6cbcf3ee743
Amadou-Marong <marongamadou6@gmail.com>
2024-02-01 19:45:33 +0000

    this is where I updated the routes and added tailwind

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,33 +1,33 @@
 <template>
-    
-    <Link href="/listing/create">New Listing</Link>
     <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-slate-800 w-full">
-        <div class="container mx-auto"></div>
-        <nav class="p-4 flex items-center justify-between">
-            <div class="text-lg font-medium">
-                <Link :href="route('listing.index')">Listings</Link>
-            </div>
-            <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
-                <!-- <Link :href="route('listing.index')">GNPC</Link> -->
-                <Link :href="route('listing.index')">
-                    GNPC
-                </Link>
-            </div>
-            <div>
-                <Link :href="route('listing.create')" class="bg-indigo-600 hover:bg-indigo-500 p-2">+ New Listing</Link>
-            </div>
-        </nav>
+        <div class="container mx-auto">
+            <nav class="p-4 flex items-center justify-between">
+                <div class="text-lg font-medium">
+                    <Link :href="route('listing.index')">Listings</Link>
+                </div>
+                <div class="text-xl text-green-600 dark:text-green-300 font-bold text-center">
+                    <!-- <Link :href="route('listing.index')">GNPC</Link> -->
+                    <Link :href="route('listing.index')">
+                        GNPC
+                    </Link>
+                </div>
+                <div>
+                    <Link :href="route('listing.create')" class="bg-green-600 hover:bg-green-500 p-2 rounded-md">+ New Listing</Link>
+                </div>
+            </nav>
+        </div>
+        
     </header>
-    <!-- <div>The Page Timer is: {{timer}}</div> -->
 
-    <!-- <div v-if="page.props.flash.success" class="alert alert-success">
-        {{page.props.flash.success}}  
-    </div> -->
-    
-    <div v-if="flashSuccess" class="alert alert-success">
-        {{flashSuccess}}  
-    </div>
-    <slot />
+    <main>
+        <div class="container mx-auto">
+            <div v-if="flashSuccess" class="alert alert-success">
+                {{flashSuccess}}  
+            </div>
+            <slot />
+        </div>
+    </main>
+
     <footer class="p-20">
         <p>copyright &copy; 2024 by Ahmad</p>
     </footer>
