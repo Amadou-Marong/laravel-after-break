95e7b3767e1810c26358d56b376167bd446a96c7
Amadou-Marong <marongamadou6@gmail.com>
2024-02-01 05:43:35 +0000

    this is where I implemented the resource controller

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -10,7 +10,7 @@
             <Link :href="`/listing/${listing.id}/edit`">Edit</Link>
         </div>
         <div>
-            <Link :href="`/listing/${listing.id}`" method="DELETE">Delete</Link>
+            <Link :href="`/listing/${listing.id}`" method="DELETE" as="button">Delete</Link>
         </div>
     </div>
 </template>
