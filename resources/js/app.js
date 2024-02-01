626783cbcc0b8db84b6d04c45944d5f4d0525cad
Amadou-Marong <marongamadou6@gmail.com>
2024-02-01 07:23:24 +0000

    this is where I updated the routes

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ import { createInertiaApp } from '@inertiajs/vue3'
 // import MainLayout from './Layouts/MainLayout.vue'
 import MainLayout from '@/Layouts/MainLayout.vue'
 import { ZiggyVue } from 'ziggy'
+import '../css/app.css'
 
 createInertiaApp({
   resolve: name => {
