92533cf9b20df160b0adea4fe6e2d8dfa364a2e3
Amadou-Marong <marongamadou6@gmail.com>
2024-02-02 00:28:02 +0000

    this is where I added the Listing comp with details

---
+++ resources/js/Components/ListingSpace.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <span class="font-bold">{{ listing.beds }} </span> bds <span class="text-gray-400">|</span> 
+        &nbsp;<span class="font-bold">{{ listing.baths }} </span> ba 
+        <span class="text-gray-400">|</span>
+        <span class="font-bold">{{ listing.area }} </span> sqft 
+       
+    </div>
+</template>
+
+<script setup>
+    defineProps({
+        listing: Object
+    })
+</script>
