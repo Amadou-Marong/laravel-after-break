1e2914db225c1eef8e17346c081b97022de9e1bc
Amadou-Marong <marongamadou6@gmail.com>
2024-02-29 12:44:21 +0000

    added confirmation for submitting offer

--- resources/js/Pages/Listing/Show/Components/MakeOffer.vue
+++ resources/js/Pages/Listing/Show/Components/MakeOffer.vue
@@ -1,58 +1,55 @@
 <template>
     <Box>
-        <template #header>Make an Offer</template>
-        <div>
-            <form @submit.prevent="makeOffer">
-                <input v-model.number="form.amount" type="text" class="input">
-                <input 
-                    v-model.number="form.amount"
-                    type="range" :min="min" :max="max" step="10000" 
-                    class="w-full mt-2 h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700">
-                <button type="submit" class="btn-outline w-full text-sm mt-2">Submit Offer</button>
-                <div class="flex justify-between text-gray-500 mt-2">
-                    <span>Difference</span>
-                    <span> <Price :price="difference" /> </span>
-                </div>
-                <span>{{form.errors.amount}}</span>
-            </form>
-        </div>
+      <template #header>Make an Offer</template>
+      <div>
+        <form @submit.prevent="confirmSubmit">
+          <input v-model.number="form.amount" type="text" class="input">
+          <input 
+            v-model.number="form.amount"
+            type="range" :min="min" :max="max" step="10000" 
+            class="w-full mt-2 h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700">
+          <button type="submit" class="btn-outline w-full text-sm mt-2">Submit Offer</button>
+          <div class="flex justify-between text-gray-500 mt-2">
+            <span>Difference</span>
+            <span> <Price :price="difference" /> </span>
+          </div>
+          <span>{{form.errors.amount}}</span>
+        </form>
+      </div>
     </Box>
-</template>
-<script setup>
-    import Box from '@/Components/UI/Box.vue';
-    import Price from '@/Components/Price.vue';
-    import {useForm} from '@inertiajs/vue3';
-    import {computed, watch} from 'vue';
-    import {debounce} from 'lodash'
-
-   const props = defineProps({
-       listingId: Number,
-       price: Number,
-   })
-
-   const form = useForm({
-       amount: props.price,
-   })
-
-   const makeOffer = () => {
-       form.post(route('listing.offer.store', 
-                {listing: props.listingId}),
-            {
-                preserveScroll: true,
-                preserveState: true,
-            }
-        )
-   }
-
-   const difference = computed(() => form.amount - props.price)
-
-   const min = computed(() => props.price / 2)
-   const max = computed(() => props.price * 2)
-
-   const emit = defineEmits(['offerUpdated'])
-
-    watch(
-        () => form.amount, 
-        debounce((value) => emit('offerUpdated', value), 200)
-    )
-</script>
+  </template>
+  
+  <script setup>
+  import Box from '@/Components/UI/Box.vue';
+  import Price from '@/Components/Price.vue';
+  import { useForm } from '@inertiajs/vue3';
+  import { computed } from 'vue';
+  
+  const props = defineProps({
+    listingId: Number,
+    price: Number,
+  });
+  
+  const form = useForm({
+    amount: props.price,
+  });
+  
+  const confirmSubmit = () => {
+    if (window.confirm('Are you sure you want to submit this offer?')) {
+      // Proceed with form submission
+      makeOffer();
+    }
+  };
+  
+  const makeOffer = () => {
+    form.post(route('listing.offer.store', { listing: props.listingId }), {
+      preserveScroll: true,
+      preserveState: true,
+    });
+  };
+  
+  const difference = computed(() => form.amount - props.price);
+  const min = computed(() => props.price / 2);
+  const max = computed(() => props.price * 2);
+  </script>
+  
